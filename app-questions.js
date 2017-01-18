a8793d26700e1baae9bd91663d19d7d367a9fe69
Joshua <Joshua.Nezianya@gmail.com>
2017-01-17 19:17:21 -0600

    production ready application

---
+++ app-questions.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        "q": "Who is the CEO of Amazon?",
+        "a": "Elizabeth Holmes",
+        "b": "Jeff Bezos",
+        "c": "Elon Musk",
+        "d": "Lebron James"
+    },
+    {
+        "q": "Who is the CEO of Apple?",
+        "a": "Tim Cook",
+        "b": "Jerry Scott",
+        "c": "Michael Dell",
+        "d": "Stephen Curry"
+    },
+    {
+        "q": "Who is the CEO of Tesla?",
+        "a": "Nikola Tesla",
+        "b": "Elon Musk",
+        "c": "Nicki Minaj",
+        "d": "Michael Jordan"
+    },
+    {
+        "q": "Who is the CEO of Sam's Club?",
+        "a": "Ursula Burns",
+        "b": "Rosalind Brewer",
+        "c": "Roslyn Sanchez",
+        "d": "Miley Cyrus"
+    }
+];
